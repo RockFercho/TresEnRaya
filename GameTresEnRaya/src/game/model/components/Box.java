bdbfa3a458c5348664bb75be29d7b2638d1fec78
Fernando Hurtado Torrico <RockFercho@gmail.com>
2014-09-10 10:04:22 -0400

    creatinf the Bos class

--- GameTresEnRaya/src/game/model/components/Box.java
+++ GameTresEnRaya/src/game/model/components/Box.java
@@ -6,9 +6,57 @@
 package game.model.components;
 
 /**
+ * Class is the box of the board.
  *
  * @author rockfercho
  */
 public class Box {
-    
+
+    /**
+     * Save the position of the box.
+     */
+    private Position position;
+    /**
+     * Saves the state of the box.
+     */
+    private State state;
+
+    /**
+     * Constructor that initializes the values ​​of global variables.
+     *
+     * @param valuePosition value for position.
+     * @param valueState value for state.
+     */
+    public Box(final Position valuePosition, final State valueState) {
+        this.position = valuePosition;
+        this.state = valueState;
+    }
+
+    /**
+     * @return the position
+     */
+    public Position getPosition() {
+        return position;
+    }
+
+    /**
+     * @param position the position to set
+     */
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    /**
+     * @return the state
+     */
+    public State getState() {
+        return state;
+    }
+
+    /**
+     * @param state the state to set
+     */
+    public void setState(State state) {
+        this.state = state;
+    }
 }
