cc681eafebdbd144365b74906ac340a52164ea72
Fernando Hurtado Torrico <RockFercho@gmail.com>
2014-09-10 09:57:05 -0400

    creating the Position class

--- GameTresEnRaya/src/game/model/components/Position.java
+++ GameTresEnRaya/src/game/model/components/Position.java
@@ -10,5 +10,39 @@ package game.model.components;
  * @author rockfercho
  */
 public class Position {
-    
+
+    /**
+     * Save the value of the position x.
+     */
+    private final int x;
+    /**
+     * Save the value of the position y.
+     */
+    private final int y;
+
+    /**
+     * Constructor manager setting values ​​for the variables.
+     *
+     * @param valueX value for x.
+     * @param valueY value for y.
+     */
+    public Position(final int valueX, final int valueY) {
+        this.x = valueX;
+        this.y = valueY;
+    }
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+
 }
