537bebb87476766279f29a991beb53e341881446
Fernando Hurtado Torrico <RockFercho@gmail.com>
2014-09-10 10:17:59 -0400

    creating the FactoryBoard class and refactory package of composition to factory

--- GameTresEnRaya/src/game/model/factory/Box.java
+++ GameTresEnRaya/src/game/model/factory/Box.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package game.model.components;
+package game.model.factory;
 
 /**
  * Class is the box of the board.
---
+++ GameTresEnRaya/src/game/model/factory/FactoryBoard.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.model.factory;
+
+/**
+ * The class is responsible for building the game board.
+ *
+ * @author rockfercho
+ */
+public class FactoryBoard {
+
+    /**
+     * Manages the various boxes initialized free.
+     */
+    private final Box boxes[][];
+
+    /**
+     * Constructor responsible for initializing global variables.
+     */
+    public FactoryBoard() {
+        this.boxes = new Box[3][3];
+        this.CreateBoxes();
+    }
+
+    /**
+     * Responsible construction method board boxes.
+     */
+    private void CreateBoxes() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                this.boxes[i][j] = new Box(new Position(i, j), State.Free);
+            }
+        }
+    }
+
+    /**
+     * @return the boxes
+     */
+    public Box[][] getBoxes() {
+        return boxes;
+    }
+}
